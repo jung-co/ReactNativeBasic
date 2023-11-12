c40896f6d517aa6ff396ae6e609b5835cf9929e6
jung-co <dal_a2@naver.com>
2023-11-12 21:29:58 +0900

    [RN2.6] Styles

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Hello! I made a RN App!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+        // {/* <StatusBar style="light"></StatusBar> */}
+      <View style={styles.container}>
+        <View style={styles.city}>
+          <Text style={styles.cityName}>Seoul</Text>
+        </View>
+        <ScrollView
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          // indicatorStyle='green'
+          contentContainerStyle={styles.weather}>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>SUNNY</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>SUNNY</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>SUNNY</Text>
+          </View>
+          <View style={styles.day}>
+            <Text style={styles.temp}>27</Text>
+            <Text style={styles.description}>SUNNY</Text>
+          </View>
+        </ScrollView>
+      </View>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1, backgroundColor: "orange"
   },
-});
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: 500
+  },
+  weather: {
+    // flex: 3,
+  },
+  day: {
+    // backgroundColor: "blue",
+    width: SCREEN_WIDTH,
+    alignItems: "center"
+  },
+  temp: {
+    marginTop: 60,
+    fontSize: 178
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60
+  }
+})
